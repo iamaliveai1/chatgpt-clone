67f8f32217020c1d670ca58b4bcbba804bc8f068
iamaliveai <iamaliveai@gmail.com>
2023-03-30 21:10:23 +0200

    Use reversed engineered chatGPT api

--- app.py
+++ app.py
@@ -1,33 +1,21 @@
-import os
-import openai
 import gradio as gr
+import openai
 
-#if you have OpenAI API key as an environment variable, enable the below
-#openai.api_key = os.getenv("OPENAI_API_KEY")
+from ailive.engine.chatgpt.chatgpt_api import ask_gpt
 
-#if you have OpenAI API key as a string, enable the below
+# if you have OpenAI API key as an environment variable, enable the below
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# if you have OpenAI API key as a string, enable the below
 openai.api_key = "xxxxxx"
 
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
 
-prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
+
 
 def openai_create(prompt):
-
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=prompt,
-    temperature=0.9,
-    max_tokens=150,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    stop=[" Human:", " AI:"]
-    )
-
-    return response.choices[0].text
-
+    return ask_gpt(prompt, delete_conversation=True)
 
 
 def chatgpt_clone(input, history):
@@ -40,16 +28,21 @@ def chatgpt_clone(input, history):
     return history, history
 
 
-block = gr.Blocks()
+def main():
+    prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
+    block = gr.Blocks()
+
+    with block:
+        gr.Markdown("""<h1><center>Build Yo'own ChatGPT with OpenAI API & Gradio</center></h1>
+        """)
+        chatbot = gr.Chatbot()
+        message = gr.Textbox(placeholder=prompt)
+        state = gr.State()
+        submit = gr.Button("SEND")
+        submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
+
+    block.launch(debug=True)
 
 
-with block:
-    gr.Markdown("""<h1><center>Build Yo'own ChatGPT with OpenAI API & Gradio</center></h1>
-    """)
-    chatbot = gr.Chatbot()
-    message = gr.Textbox(placeholder=prompt)
-    state = gr.State()
-    submit = gr.Button("SEND")
-    submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
-
-block.launch(debug = True)
+if __name__ == "__main__":
+    main()
